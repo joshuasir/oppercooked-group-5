db0fba5516f754221773bf3d606238428a4986ee
joshua sirusstara <71873035+joshuasir@users.noreply.github.com>
2020-11-21 20:16:51 +0700

    Update welcome-fix.cpp

--- oppercooked-group-5/welcome-fix.cpp
+++ oppercooked-group-5/welcome-fix.cpp
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+/* dummy struct
+struct menu{
+	char name[55];
+	char price[55];
+	char topping[55];
+	char callories[55];
+	char flavor[55];
+	char size[55];
+	menu *next;
+	menu *prev;
+}*head,*tail,*curr;
+*/
 void mainMenu(int profit);
 int scanMainMenu();
 void ChooseFoodToAdd();
@@ -148,4 +159,51 @@ int totalTimeDrink(char flavor){
         return x + 20;
     }
     
+}
+
+int printOrder(){
+	if(!head){
+		printf("There is no Dessert or Drink on the list!\nPress Enter to continue\n");
+        getchar();
+        return 0;
+	}
+     int i=1;
+     curr=head;
+     char format[]="| %-5s | %-10s | %-5s | %-10s | %-5s | %-10s | %-5s |\n";
+     printf(format,"No","Name","Price","Topping","Callories","Flavor","Size");
+     for(int j=0;j<=75;j++){
+           printf("-");
+     }
+     puts("");
+     while(curr){
+        printf(format,i,curr->name,curr->price,curr->topping,curr->callories,curr->flavor,curr->size); 
+    	curr=curr->next;
+    	i++;
+	}
+    return i;
+}
+menu *findMenu(int num){
+	if(!head){
+		return NULL
+	}else{
+		int i=1;
+		curr=head;
+		while(curr&&i<num){
+			curr=curr->next;
+			i++;
+		}
+		return curr;
+	}
+}
+void Order(){
+    int choose,quantity=printOrder();
+    if(quantity){
+        do{
+        scanf("%d",&choose);
+        getchar();
+        }while(choose>quantity);
+        cook(findMenu(choose));    //function cooking
+        printf("Successfully add to order list!\n\nPress Enter to continue");
+        getchar();}
+    return;
 }
